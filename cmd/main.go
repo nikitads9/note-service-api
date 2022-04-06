6ed210da05669de67a12fa32460fb53eb7d29d5a
Nikita Denisenok <nikitadenisenok@gmail.com>
2022-04-06 18:28:09 +0300

    fix issues with task1

--- cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-//import "github.com/nikitads9/note-service-api/utils"
-
-func main() {
-	/*someSlice := []int64{1, 2, 3, 4, 5, 6}
-	someFilter := []int64{2, 5}
-	utils.SliceFilter(someSlice, someFilter) */
-}
--- internal/app/api/note_v1/note_v1.go
+++
@@ -1,7 +0,0 @@
-package note_v1
-
-import desc "github.com/nikitads9/note-service-api/pkg/note_api"
-
-type Implementation struct {
-	desc.UnimplementedNoteV1Server
-}
